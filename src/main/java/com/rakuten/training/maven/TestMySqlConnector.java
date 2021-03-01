25385c63696d07bcbcc5ef57a2de1f56efd8af4b
ShilpaRajput123 <shilpabhadwal2@gmail.com>
2021-03-01 11:02:53 +0530

    commit msg

---
+++ src/main/java/com/rakuten/training/maven/TestMySqlConnector.java
@@ -0,0 +1,12 @@
+package com.rakuten.training.maven;
+import com.mysql.jdbc.Driver;
+public class TestMySqlConnector {
+
+	public static void main(String[] args) {
+		Driver d;
+		System.out.println("Driver works!");
+		
+		
+	}
+
+}
